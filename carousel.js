8b35dc7fd36e3776c045779c67b9f947370c15a1
Manish <manihs@gmail.com>
2020-09-03 19:29:24 +0545

    Working in JS, When click on left or right button image changed.

--- carousel.js
+++ carousel.js
@@ -15,3 +15,29 @@ function positionSlides(slides) {
   }
 }
 positionSlides(slides);
+
+//Select Next Button
+const nextButton = document.querySelector(".right-btn");
+
+//On right click button, we move(translateX) the carousel to the left position
+nextButton.addEventListener("click", function () {
+  const currentSlide = carousel.querySelector(".active");
+  const nextSlide = currentSlide.nextElementSibling;
+  const position = nextSlide.style.left;
+  carousel.style.transform = `translateX(-${position})`;
+  currentSlide.classList.remove("active");
+  nextSlide.classList.add("active");
+});
+
+//Select previous button.
+const previousButton = document.querySelector(".left-btn");
+
+//On the left button click, we move(translateX) carousel to the right position.
+previousButton.addEventListener("click", function () {
+  const currentSlide = carousel.querySelector(".active");
+  const previousSlide = currentSlide.previousElementSibling;
+  const position = previousSlide.style.left;
+  carousel.style.transform = `translateX(-${position})`;
+  currentSlide.classList.remove("active");
+  previousSlide.classList.add("active");
+});
